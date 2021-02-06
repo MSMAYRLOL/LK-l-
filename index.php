af82964bf4a8a5acb68ab153bc34ea1614dc66d7
MSMAYRLOL <78488356+MSMAYRLOL@users.noreply.github.com>
2021-02-06 20:19:55 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Мой сайт';
+$name = 'Неопознанный Программист';
+$image = 'img/unknown-raccoon.svg';
+$email = 'mishasheronov@gmail.com';
+$phone = '+79520210079';
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
